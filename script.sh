80f6f4460c06312f04a0a0900a84ee7209f0fe53
Restyled.io <commits@restyled.io>
2023-01-15 23:03:01 +0000

    Restyled by shellharden

--- script.sh
+++ script.sh
@@ -50,7 +50,7 @@ If you want to **contribute** or you have found a problem, you can do it by crea
 - [Semester 4](/docs/category/semester-4)" >> "docs/intro.md"
 
 # Parcours récursif des dossiers dans subjects
-for dir in $(find $src_path -type d); do
+for dir in "$(find "$src_path" -type d)"; do
     # Création du chemin relatif du dossier courant
     if [[ $dir == *".git"* || $dir == *".github"* ]]; then
       continue
@@ -60,7 +60,7 @@ for dir in $(find $src_path -type d); do
     abs_path="$dst_path/$rel_path"
 
     # Vérifie s'il existe un fichier pdf ou .md dans le dossier courant
-    if [ -n "$(find $dir -maxdepth 1 -name '*.pdf')" ]; then
+    if [ -n "$(find "$dir" -maxdepth 1 -name '*.pdf')" ]; then
         # Création du fichier .mdx avec le nom du dossier courant
         IFS='/' read -ra ADDR <<< "$rel_path"
         echo -e "---\ntitle: ${abs_path##*/}\ntags:" >> "$abs_path.mdx"
@@ -70,7 +70,7 @@ for dir in $(find $src_path -type d); do
         echo -e "---\n> Timeline: ?\n\n" >> "$abs_path.mdx"
         # Ajout de tous les fichiers dans le dossier courant dans le fichier .mdx
         echo -e "## Files 📂\n\n:::note 2020 Subject and Files" >> "$abs_path.mdx"
-        find $dir -maxdepth 1 -type f -not -name '*.md' -not -name 'tests.txt' -exec bash -c 'string={}; filename=$(basename "$string"); echo "- [$filename]($string)"' \; >> "$abs_path.mdx"
+        find "$dir" -maxdepth 1 -type f -not -name '*.md' -not -name 'tests.txt' -exec bash -c 'string={}; filename=$(basename "$string"); echo "- [$filename]($string)"' \; >> "$abs_path.mdx"
         echo -e ":::\n" >> "$abs_path.mdx"
         if [ -f "$dir/tests.txt" ]; then
             # Ajout du contenu du fichier tests.txt dans le fichier .mdx
@@ -81,7 +81,7 @@ for dir in $(find $src_path -type d); do
         sed -i '' -e 's|./subjects/|https://github.com/Studio-17/Epitech-Subjects/raw/main/|g' "$abs_path.mdx"
     else
         # Création du dossier courant dans docs
-        mkdir -p $abs_path
+        mkdir -p "$abs_path"
         # Création du fichier category.json dans le dossier courant
         touch "$abs_path/_category_.json"
         # Ecrire le nom du dossier courant dans le fichier category.json
