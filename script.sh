bca88fea89bf356d1eab51385f3cf63bd19e0ee6
Clément FERNANDES <clement.fernandes@epitech.eu>
2023-01-16 00:01:37 +0100

    fix(intro): added a intro.md

--- script.sh
+++ script.sh
@@ -17,6 +17,38 @@ dst_path="./docs"
 
 touch "docs/intro.md"
 
+echo "---
+sidebar_position: 1
+---
+
+# Intro
+
+![Studio 17](https://img.shields.io/badge/Github-Studio--17-06DFF9)
+![Version](https://img.shields.io/badge/Release-v0.5beta-ff0000)
+![visitor badge](https://visitor-badge.glitch.me/badge?page_id=Studio-17.Epitech-Subjects)
+
+Welcome to the **Epitech Subjects** website ! This directory contains all topics and files related to Epitech PGE program.
+
+:::important Note
+**This website is not official and is not related to Epitech in any way.**
+
+All the files are not yet up to date (they will be gradually)
+:::
+
+:::tip Featuring
+The valid credits, the timeline, the tests (details of the Marvin tests, TAM, DEFENSE) for these projects are also indicated by clicking on the links.
+
+If you want to **contribute** or you have found a problem, you can do it by creating a issue [Here](https://github.com/Studio-17/Epitech-Subjects/issues).
+:::
+
+## All these semesters are available
+
+- [Semester 0](/docs/category/semester-0)
+- [Semester 1](/docs/category/semester-1)
+- [Semester 2](/docs/category/semester-2)
+- [Semester 3](/docs/category/semester-3)
+- [Semester 4](/docs/category/semester-4)" >> "docs/intro.md"
+
 # Parcours récursif des dossiers dans subjects
 for dir in $(find $src_path -type d); do
     # Création du chemin relatif du dossier courant
@@ -31,7 +63,7 @@ for dir in $(find $src_path -type d); do
     if [ -n "$(find $dir -maxdepth 1 -name '*.pdf')" ]; then
         # Création du fichier .mdx avec le nom du dossier courant
         IFS='/' read -ra ADDR <<< "$rel_path"
-        echo -e "---\ntitle: Firtree\ntags:" >> "$abs_path.mdx"
+        echo -e "---\ntitle: ${abs_path##*/}\ntags:" >> "$abs_path.mdx"
         for i in "${ADDR[@]}"; do
           echo "  - $i" >> "$abs_path.mdx"
         done
@@ -40,9 +72,6 @@ for dir in $(find $src_path -type d); do
         echo -e "## Files 📂\n\n:::note 2020 Subject and Files" >> "$abs_path.mdx"
         find $dir -maxdepth 1 -type f -not -name '*.md' -not -name 'tests.txt' -exec bash -c 'string={}; filename=$(basename "$string"); echo "- [$filename]($string)"' \; >> "$abs_path.mdx"
         echo -e ":::\n" >> "$abs_path.mdx"
-            echo -e "## Tests 🤖\n<details>\n  <summary>Toggle to see the tests</summary>\n  <div>" >> "$abs_path.mdx"
-            echo "Yo" >> "$abs_path.mdx"
-            echo -e "  </div>\n</details>\n" >> "$abs_path.mdx"
         if [ -f "$dir/tests.txt" ]; then
             # Ajout du contenu du fichier tests.txt dans le fichier .mdx
             echo -e "## Tests 🤖\n<details>\n  <summary>Toggle to see the tests</summary>\n  <div>" >> "$abs_path.mdx"
@@ -56,9 +85,10 @@ for dir in $(find $src_path -type d); do
         # Création du fichier category.json dans le dossier courant
         touch "$abs_path/_category_.json"
         # Ecrire le nom du dossier courant dans le fichier category.json
-        echo "{\"label\": \"$rel_path\",\"link\": {\"type\": \"generated-index\", \"description\": \"To define\"}}" >> "$abs_path/_category_.json"
+        echo "{\"label\": \"${rel_path##*/}\",\"link\": {\"type\": \"generated-index\", \"description\": \"To define\"}}" >> "$abs_path/_category_.json"
     fi
 done
 
 # Suppression du dossier subjects
 rm -rf subjects
+rm -rf docs/subjects
